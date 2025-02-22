df65388aa34b4b3c8e843fddcc43962554b5444b
Vincent Cina <vcina@me.com>
2025-02-22 17:01:31 -0500

    Initial Commit

---
+++ TwoWayBindings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwoWayBindings
+//
+//  Created by VINCENT CINA on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by VINCENT CINA on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
