11dd19463c8e322ccd79aa58704054f16cf9f7f3
Vincent Cina <vcina@me.com>
2025-02-22 17:44:19 -0500

    Added colorPicker, TextField, DatePicker, Stepper etc. controls

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pressCount = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .red
+    @State private var selectedDate: Date = Date()
+    @State private var stepperValue = 1
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("Press count: \(pressCount)")
+
+            Button("Press Me") {
+                pressCount += 1
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            TextField("Enter a name:", text: $name)
+                .textFieldStyle(.roundedBorder)
+            Text("Neme entered: \(name)")
+            Spacer()
+            
+            Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
+            Spacer()
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            ColorPicker("Pick a color:", selection: $selectedColor)
+            Spacer()
+            DatePicker("Date:", selection: $selectedDate)
+            Text("Selected date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            Spacer()
+            Stepper("Stepper value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            Spacer()
         }
+        .font(.title2)
         .padding()
+        Spacer()
     }
 }
 
